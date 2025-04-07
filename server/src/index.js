9af8b0d53b212a5ced48496dd006e757cd3c8329
harry-rollnum <harry.fronz@gmail.com>
2025-04-07 20:58:10 +0530

    added whole code

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://chaosrider870:fK8rmguOcvNCq0TY@cluster0.g9bzzhx.mongodb.net/recipe-db?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
